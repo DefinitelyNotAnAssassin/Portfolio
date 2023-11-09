b6cdab9489a668bc6b93e84ac06316bbe7e5f619
DefinitelyNotAnAssassin <dharklike@gmail.com>
2023-11-10 02:26:29 +0800

    Terminal Portfolio

--- app.py
+++ app.py
@@ -6,7 +6,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('terminal.html')
+
+
+
+
+@app.route('/terminal')
+def terminal():
+   
+    command =  request.args['terminal']
+    back_log = f"$ {command}<br>"
+    if  command == "-help" or command == '-h':
+        return """
+Commands Available: 
+    <br>
+    -help , -h      -   Produces the list of available commands
+    <br>    
+
+"""
+    else:
+        return f"{back_log}'{command}' is not recognized as an internal or external command, operable program or batch file. <br><br>"
+
 
 @app.route('/hello')
 def hello():
